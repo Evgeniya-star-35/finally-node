d732f0f655ef599c2a3a8eeb6addb2374583141c
Evgeniya Bayda <baje75128@gmail.com>
2022-02-02 19:03:20 +0200

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+
+import { HttpCode, LIMIT_JSON } from "./lib/constants";
+
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(helmet());
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json({ limit: LIMIT_JSON }));
+app.use((req, res, next) => {
+  app.set("lang", req.acceptsLanguages(["en", "ru"]));
+  next();
+});
+app.use((req, res) => {
+  res
+    .status(HttpCode.NOT_FOUND)
+    .json({ status: "error", code: HttpCode.NOT_FOUND, message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+    status: "fail",
+    code: HttpCode.INTERNAL_SERVER_ERROR,
+    message: err.message,
+  });
+});
+
+export default app;
---
+++ bin/server.js
@@ -0,0 +1,13 @@
+import { mkdir } from "fs/promises";
+import app from "../app";
+import db from "../lib/db";
+
+const PORT = process.env.PORT || 5050;
+
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+}).catch((err) => {
+  console.log(`Server not running. Error: ${err.message}`);
+});
