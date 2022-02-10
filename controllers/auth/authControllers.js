56c3195b64b82c48c26e37c28b403e546ddfba23
Николай Гребенюк <Yanukhous@ukr.net>
2022-02-10 17:04:54 +0200

    fix

--- controllers/auth/authControllers.js
+++ controllers/auth/authControllers.js
@@ -3,7 +3,6 @@ const queryString = require("query-string");
 const { HttpCode } = require("../../lib/constants");
 const authService = require("../../service/auth");
 const Users = require("../../repository/users");
-const User = require("../../model/user");
 // const EmailService = require("../../service/email/service");
 // const { CreateSenderSendGrid } = require("../../service/email/sender");
 
@@ -135,64 +134,6 @@ class AuthControllers {
     }
   }
 
-  // async googleAuth(_req, res) {
-  //   const stringifiedParams = queryString.stringify({
-  //     client_id: process.env.GOOGLE_CLIENT_ID,
-  //     redirect_uri: `${process.env.BACK_BASE}/api/users/google-redirect`,
-  //     scope: [
-  //       "https://www.googleapis.com/auth/userinfo.email",
-  //       "https://www.googleapis.com/auth/userinfo.profile",
-  //     ].join(" "),
-  //     response_type: "code",
-  //     access_type: "offline",
-  //     prompt: "consent",
-  //   });
-  //   return res.redirect(
-  //     `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
-  //   );
-  // }
-  // async googleRedirect(req, res) {
-  //   const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-  //   const urlObj = new URL(fullUrl);
-  //   const urlParams = queryString.parse(urlObj.search);
-
-  //   const code = urlParams.code;
-
-  //   const tokenData = await axios({
-  //     url: "https://oauth2.googleapis.com/token",
-  //     method: "post",
-  //     data: {
-  //       client_id: process.env.GOOGLE_CLIENT_ID,
-  //       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-  //       redirect_uri: `${process.env.BACK_BASE}/api/users/google-redirect`,
-  //       grant_type: "authorization_code",
-  //       code: code,
-  //     },
-  //   });
-
-  //   const userData = await axios({
-  //     url: "https://www.googleapis.com/oauth2/v2/userinfo",
-  //     method: "get",
-  //     headers: {
-  //       Authorization: `Bearer ${tokenData.data.access_token}`,
-  //     },
-  //   });
-
-  //   const isUserExist = await authService.isExistUser(userData.data.email);
-
-  //   if (!isUserExist) {
-  //     await authService.createUser({ email: userData.data.email });
-  //   }
-
-  //   const currentUser = await User.findOne({ email: userData.data.email });
-
-  //   return res.redirect(
-  //     `${process.env.FRONTEND_URL}?token=${currentUser.token}`
-  //   );
-
-  //   // return res.?redirect(`${process.env.FRONTEND_URL}/main`);
-  // }
-
   async googleAuth(_req, res, next) {
     try {
       const stringifiedParams = queryString.stringify({
@@ -248,10 +189,8 @@ class AuthControllers {
         const idUser = newUser.id;
         await Users.updateGoogleUser(idUser, picture);
         const token = Users.createToken(idUser);
-        console.log("token:", token);
         const refreshToken = Users.createRefreshToken(idUser);
         const userToken = await Users.updateToken(idUser, token, refreshToken);
-        console.log("userToken:", userToken);
         return res.redirect(
           `${process.env.FRONTEND_URL}?token=${userToken.token}&refreshToken=${refreshToken}`
         );
