d436deaa9613143086a970d7bb7798f5180de6de
Николай Гребенюк <Yanukhous@ukr.net>
2022-02-11 20:07:08 +0200

    add google

--- controllers/google/auth-google.js
+++ controllers/google/auth-google.js
@@ -82,7 +82,7 @@ const googleRedirect = async (req, res, next) => {
     const refreshToken = Users.createRefreshToken(idUser);
     await Users.updateToken(idUser, token, refreshToken);
     return res.redirect(
-      `${process.env.FRONTEND_URL}?token=${token}&refreshToken=${refreshToken}`
+      `${process.env.FRONTEND_URL}/googleAuth?token=${token}&refreshToken=${refreshToken}`
     );
   } catch (error) {
     next(error);
