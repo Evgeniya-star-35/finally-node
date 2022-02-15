a82e67579a0fde00835eeecd47595cfbc7d1fbf1
Николай Гребенюк <Yanukhous@ukr.net>
2022-02-15 18:11:16 +0200

    fix

--- service/email/service.js
+++ service/email/service.js
@@ -6,13 +6,13 @@ class EmailService {
 
     switch (env) {
       case "development":
-        this.link = process.env.SEND_EMAIL_URL;
+        this.link = process.env.BASE_URL;
         break;
       case "production":
         this.link = process.env.BACK_BASE;
         break;
       default:
-        this.link = process.env.FRONTEND_URL;
+        this.link = process.env.LINK_HOST_DEFAULT;
         break;
     }
   }
@@ -40,8 +40,8 @@ class EmailService {
           button: {
             color: "#FF751D",
             text: "Confirm your account",
-            link: `${this.link}`,
-            // link: `${this.link}/api/users/verify/${verifyToken}`,
+            // link: `${this.link}`,
+            link: `${this.link}/api/users/verify/${verifyToken}`,
           },
         },
         outro:
